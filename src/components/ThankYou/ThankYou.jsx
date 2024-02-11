e09bffd8e17ed4af1709e3874dd0f44445967bf5
Ying Her <yher.prime@gmail.com>
2024-02-11 14:25:22 -0600

    updated thank you page to have the necessary fields and create button

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -1,9 +1,22 @@
 function ThankYou() {
+  const handleClickResetSurvey = (event) => {
+    // TODO: Write a function that will navigate to the beginning of the survey
+    // when clicked on the "Leave New Feedback" and reset/delete the previous survey data
+    event.preventDefault();
+    console.log('event', event);
+    // make axio call - .then/refresh/.catch
+  };
+
   return (
     <>
-      {/* // TODO: Created header tag "Feedback!"
-// <p> Thank You! </p>
-  // TODO: <button onClick of 'Leave New Feedback' />  */}
+      {/* TODO: Created header tag "Feedback!" */}
+
+      {/* <p> Thank You! </p> */}
+      <p>Thank You!</p>
+      {/* TODO: <button onClick of 'Leave New Feedback' /> */}
+      <button onClick={(event) => handleClickResetSurvey}>
+        Leave New Feedback
+      </button>
     </>
   );
 }
