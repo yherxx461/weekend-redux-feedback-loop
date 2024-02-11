a67db6675171a5b24f6d0cb8398fb8216076120c
Ying Her <yher.prime@gmail.com>
2024-02-11 11:52:48 -0600

    commented to-do list for thank you component

---
+++ src/components/ThankYou/ThankYou.jsx
@@ -0,0 +1,11 @@
+function ThankYou() {
+  return (
+    <>
+      {/* // TODO: Created header tag "Feedback!"
+// <p> Thank You! </p>
+  // TODO: <button onClick of 'Leave New Feedback' />  */}
+    </>
+  );
+}
+
+export default ThankYou;
