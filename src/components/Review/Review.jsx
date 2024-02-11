a941ed25c3cb58ee961ec4a691e397bf0446336a
Ying Her <yher.prime@gmail.com>
2024-02-11 12:58:50 -0600

    fields created for how Review page should look like + button

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,9 +1,17 @@
 function Review() {
   return (
     <>
-      {/* TODO: Create <p> tag "Review Your Feedback"
-TODO: List out feelings, understanding, support, and comments values.
-TODO: <Submit Button> */}
+      {/* TODO: Create <p> tag "Review Your Feedback" */}
+      <h3>Review Your Feedback</h3>
+      {/* TODO: List out feelings, understanding, support, and comments values. */}
+      <p>Feelings: {`feeling.value`} </p>
+      <p>Understanding: {`understanding.value`}</p>
+      <p>Support: {`support.value`}</p>
+      <p>Comments: {`comment.value`}</p>
+      {/* TODO: <Submit Button> */}
+      <button type="submit" data-testid="next">
+        Next
+      </button>
     </>
   );
 }
