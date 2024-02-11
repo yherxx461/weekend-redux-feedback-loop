a980e0cd9574bce7237201cf99f23265ca15cc5c
Ying Her <yher.prime@gmail.com>
2024-02-11 11:58:36 -0600

    created review component with todo list of what is needed

---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,11 @@
+function Review() {
+  return (
+    <>
+      {/* TODO: Create <p> tag "Review Your Feedback"
+TODO: List out feelings, understanding, support, and comments values.
+TODO: <Submit Button> */}
+    </>
+  );
+}
+
+export default Review;
