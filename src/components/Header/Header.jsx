158f44885cee35ba15fc174c933ff6dfed07e2a7
Ying Her <yher.prime@gmail.com>
2024-02-11 12:20:03 -0600

    Header component created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <>
+      <header className="Header">
+        <h1 className="Header-title">Feedback!</h1>
+        <h4>Don't forget it!</h4>
+      </header>
+    </>
+  );
+}
+
+export default Header;
