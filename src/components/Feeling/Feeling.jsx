175090a7c8905885580ac411556b05088bd0914d
Ying Her <yher.prime@gmail.com>
2024-02-11 11:42:43 -0600

    commented to-do list for feeling component

---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,12 @@
+function Feeling() {
+  return (
+    <>
+      {/* // TODO: Created header tag "Feedback!"
+  // TODO: progress bar with of current page (ex: 1 of 4 pages)
+  // TODO: "How are you feeling today?"
+  // TODO: <'Next Button'> */}
+    </>
+  );
+}
+
+export default Feeling;
