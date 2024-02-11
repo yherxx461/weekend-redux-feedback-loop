d296973fdd42428449d9e819d2290bdcc99ea262
Ying Her <yher.prime@gmail.com>
2024-02-11 11:49:15 -0600

    commented to-do list for comments component

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,13 @@
+function Comments() {
+  return (
+    <>
+      {/* // TODO: Created header tag "Feedback!"
+  // TODO: progress bar with of current page (ex: 4 of 4 pages)
+  // TODO: "Any comments you want to leave?"
+  // TODO: <input tag for text>
+  // TODO: <'Next Button'> and <check mark indicating done>*/}
+    </>
+  );
+}
+
+export default Comments;
