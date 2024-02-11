3b018b011d35c9e4f4624e1d01b9122234ad4a39
Ying Her <yher.prime@gmail.com>
2024-02-11 16:23:47 -0600

    modified fields in ThankYou file + alert button work

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom/cjs/react-router-dom';
 
-function ThankYou() {
+function thankYou() {
   const handleClickNavToSurvey = () => {
     // TODO: Write a function that will navigate to the beginning of the survey
     // when clicked on the "Leave New Feedback" and reset/delete the previous survey data
-    alert('Clicked To Nav');
+    alert('Your feedback will be reset.');
     history.pushState('/feeling');
     // make axio call - .then/refresh/.catch
   };
@@ -14,13 +14,11 @@ function ThankYou() {
       {/* TODO: Created header tag "Feedback!" */}
 
       {/* <p> Thank You! </p> */}
-      <p>Thank You!</p>
+      <h2>Thank You!</h2>
       {/* TODO: <button onClick of 'Leave New Feedback' /> */}
-      <Link to="/feeling">
-        <button onClick={handleClickNavToSurvey}>Leave New Feedback</button>
-      </Link>
+      <button onClick={handleClickNavToSurvey}>Leave New Feedback</button>
     </>
   );
 }
 
-export default ThankYou;
+export default thankYou;
