8cc1d923a0afb3be2f38910fdd2e2bee88fb366b
Ying Her <yher.prime@gmail.com>
2024-02-12 18:50:38 -0600

    creating POST ROUTE to receive data

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool')
-
+const pool = require('../modules/pool');
 
 // TODO: This route adds a new feedback entry
 router.post('/', (req, res) => {
+  console.log('POST ROUTER');
 
-})
+  let queryText =
+    'INSERT INTO "feedback" ("feeling", "understanding", "support", "comment") VALUES ($1, $2, @3, $4);';
 
+  pool
+    .query(queryText, [
+      req.body.feeling,
+      req.body.understanding,
+      req.body.support,
+      req.body.comments,
+    ])
+    .then((result) => {
+      //SUCCESS
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.error('ERROR in POST ROUTE', error);
+      res.sendStatus(500);
+    });
+});
 
 // DO NOT EDIT THIS ROUTE
 // This route must return all feedback.
 router.get('/', (req, res) => {
-    console.log('testing')
-    const sqlText = `SELECT * FROM "feedback" ORDER BY "id"`;
-    pool.query(sqlText).then(result => {
-        res.send(result.rows)
-    }).catch(err => {
-        console.log(err);
-        res.sendStatus(500);
+  console.log('testing');
+  const sqlText = `SELECT * FROM "feedback" ORDER BY "id"`;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
