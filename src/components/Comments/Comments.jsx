97ff874a5ad4c4e78231bb7aaa20d75cc38fb596
Ying Her <yher.prime@gmail.com>
2024-02-13 05:56:16 -0600

    edited fields in comment components, however, comment value is not returned

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,30 +1,44 @@
 import { Button, Link } from '@mui/material';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Comments() {
   // TODO: Write a function to submit the comment and clear field -- no required
+  const [commentsResponse, setCommentsResponse] = useState('');
+  const dispatch = useDispatch();
+
+  function C_handleClickNext(event) {
+    console.log('Comment Response', { commentsResponse });
+    dispatch({ type: 'SET_COMMENTS_RESPONSE', payload: commentsResponse });
+  }
   return (
     <>
       {/* // TODO: Created header tag "Feedback!" ---> no longer needed as it's only from the admin side of things */}
       {/* TODO: progress bar with of current page (ex: 4 of 4 pages) ---> no longer needed as it's only from the admin side of things */}
 
       {/* TODO: "Any comments you want to leave?" */}
-      <form data-testid="input">
-        <h2>Any comments you want to leave?</h2>
+      <div data-testid="input">
+        <h2>Do you have any comments or concerns?</h2>
         <p>Comments</p>
         {/* TODO: <input tag for text> */}
-        <input type="text"></input>
+        <input
+          type="text"
+          value={commentsResponse}
+          onChange={(event) => setCommentsResponse(event.target.value)}
+        ></input>
         {/* TODO: <'Next Button'> and <check mark indicating done> */}
         <Link to="/review">
           <Button
+            data-testid="next"
             variant="outlined"
             size="small"
             type="submit"
-            data-testid="next"
+            onCanPlay={C_handleClickNext}
           >
             Next
           </Button>
         </Link>
-      </form>
+      </div>
     </>
   );
 }
