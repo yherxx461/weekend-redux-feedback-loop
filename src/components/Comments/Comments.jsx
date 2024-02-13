c99329cbce91e51b73e2551310838e582c76e410
Ying Her <yher.prime@gmail.com>
2024-02-13 08:04:14 -0600

    Support and Comment component linked to one another

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,4 +1,5 @@
-import { Button, Link } from '@mui/material';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -7,38 +8,46 @@ function Comments() {
   const [commentsResponse, setCommentsResponse] = useState('');
   const dispatch = useDispatch();
 
-  function C_handleClickNext(event) {
+  const handleCommentsToNext = (event) => {
     console.log('Comment Response', { commentsResponse });
     dispatch({ type: 'SET_COMMENTS_RESPONSE', payload: commentsResponse });
-  }
+    // clear fields
+    setCommentsResponse('');
+  };
+  const handleCommentsUpdate = (event) => {
+    console.log(feelingResponse);
+    setCommentsResponse(event.target.value);
+  };
   return (
     <>
       {/* // TODO: Created header tag "Feedback!" ---> no longer needed as it's only from the admin side of things */}
       {/* TODO: progress bar with of current page (ex: 4 of 4 pages) ---> no longer needed as it's only from the admin side of things */}
 
       {/* TODO: "Any comments you want to leave?" */}
-      <div data-testid="input">
+      <main>
         <h2>Do you have any comments or concerns?</h2>
-        <p>Comments</p>
-        {/* TODO: <input tag for text> */}
-        <input
-          type="text"
-          value={commentsResponse}
-          onChange={(event) => setCommentsResponse(event.target.value)}
-        ></input>
-        {/* TODO: <'Next Button'> and <check mark indicating done> */}
-        <Link to="/review">
-          <Button
-            data-testid="next"
-            variant="outlined"
-            size="small"
-            type="submit"
-            onCanPlay={C_handleClickNext}
-          >
-            Next
-          </Button>
-        </Link>
-      </div>
+        <form>
+          <p>Comments</p>
+          {/* TODO: <input tag for text> */}
+          <input
+            data-testid="input"
+            type="text"
+            onChange={handleCommentsToNext}
+          ></input>
+          {/* TODO: <'Next Button'> and <check mark indicating done> */}
+          <Link to="/review">
+            <Button
+              data-testid="next"
+              variant="outlined"
+              size="small"
+              defaultValue={commentsResponse}
+              onClick={handleCommentsUpdate}
+            >
+              Next
+            </Button>
+          </Link>
+        </form>
+      </main>
     </>
   );
 }
--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,4 +1,5 @@
-import { Button, Link } from '@mui/material';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -6,41 +7,47 @@ function Support() {
   const [supportResponse, setSupportResponse] = useState(0);
   const dispatch = useDispatch();
 
-  function S_handleClickNext() {
+  const handleSupportToNext = (event) => {
     console.log('Support Response', { supportResponse });
     dispatch({
       type: 'SET_SUPPORT_RESPONSE',
       payload: parseInt(supportResponse),
     });
-  }
+    // clear fields
+    setSupportResponse('');
+  };
+  const handleSupportUpdate = (event) => {
+    console.log(supportResponse);
+    setSupportResponse(event.target.value);
+  };
   return (
     <>
       {/* TODO: Created header tag "Feedback!" ---> no longer needed as it's only from the admin side of things */}
       {/* TODO: progress bar with of current page (ex: 3 of 4 pages) ---> no longer needed as it's only from the admin side of things */}
       {/* TODO: "How well are you being supported?" */}
-      <div data-testid="input">
+      <main className="support">
         <h2>How well are you being supported?</h2>
-        <p>Support?</p>
-        <input
-          type="number"
-          min={0}
-          max={5}
-          value={supportResponse}
-          onChange={(event) => setSupportResponse(event.target.value)}
-        ></input>
-        {/* TODO: <'Next Button'> */}
-        <Link to="/comments">
+        <form>
+          <p>Support?</p>
+          <input
+            data-testid="input"
+            type="number"
+            min={0}
+            max={5}
+            value={supportResponse}
+            onChange={handleSupportUpdate}
+          ></input>
+          {/* TODO: <'Next Button'> */}
           <Button
             data-testid="next"
             variant="outlined"
             size="small"
-            type="submit"
-            onClick={S_handleClickNext}
+            onClick={handleSupportToNext}
           >
-            Next
+            <Link to="/comments">Next</Link>
           </Button>
-        </Link>
-      </div>
+        </form>
+      </main>
     </>
   );
 }
