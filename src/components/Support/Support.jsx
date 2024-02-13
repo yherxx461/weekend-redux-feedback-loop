ded63beb087548d8196c101e6603dc66db15df21
Ying Her <yher.prime@gmail.com>
2024-02-13 06:03:50 -0600

    Support Components rendering value

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,6 +1,18 @@
 import { Button, Link } from '@mui/material';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Support() {
+  const [supportResponse, setSupportResponse] = useState(0);
+  const dispatch = useDispatch();
+
+  function S_handleClickNext() {
+    console.log('Support Response', { supportResponse });
+    dispatch({
+      type: 'SET_SUPPORT_RESPONSE',
+      payload: parseInt(supportResponse),
+    });
+  }
   return (
     <>
       {/* TODO: Created header tag "Feedback!" ---> no longer needed as it's only from the admin side of things */}
@@ -9,10 +21,22 @@ function Support() {
       <div data-testid="input">
         <h2>How well are you being supported?</h2>
         <p>Support?</p>
-        <input type="number"></input>
+        <input
+          type="number"
+          min={0}
+          max={5}
+          value={supportResponse}
+          onChange={(event) => setSupportResponse(event.target.value)}
+        ></input>
         {/* TODO: <'Next Button'> */}
         <Link to="/comments">
-          <Button type="submit" data-testid="next">
+          <Button
+            data-testid="next"
+            variant="outlined"
+            size="small"
+            type="submit"
+            onClick={S_handleClickNext}
+          >
             Next
           </Button>
         </Link>
