8d3287d50c0f88a41550888482a82bb8fe77ca50
Ying Her <yher.prime@gmail.com>
2024-02-14 07:23:22 -0600

    updated the history function to ensure all history is push to the database

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -1,12 +1,13 @@
 import { Button } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 
 function thankYou() {
+  const history = useHistory();
   // TODO: Write a function that will navigate to the beginning of the survey
   // when clicked on the "Leave New Feedback" and reset/delete the previous survey data
-  const handleClickNavToSurvey = () => {
+  const handleNewFeedback = () => {
     alert('Your feedback will be reset.');
-    history.pushState('/feeling');
-    // make axio call - .then/refresh/.catch
+    history.push('/');
   };
 
   return (
@@ -15,11 +16,9 @@ function thankYou() {
 
       {/* <p> Thank You! </p> */}
       <h2>Thank You!</h2>
-      <p id="thankyou">
-        You've completed the survey! We appreciate your feedback!
-      </p>
+      <p>You've completed the survey! We appreciate your feedback!</p>
       {/* TODO: <button onClick of 'Leave New Feedback' /> */}
-      <Button onClick={handleClickNavToSurvey}>Leave New Feedback</Button>
+      <Button onClick={handleNewFeedback}>Leave New Feedback</Button>
     </>
   );
 }
