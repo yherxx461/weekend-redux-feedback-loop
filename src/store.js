fef3290a794c7822f510ebf5c63ad5461ac941f1
Ying Her <yher.prime@gmail.com>
2024-02-14 05:23:49 -0600

    modified store files as it was not initilizing the responses per console error

--- src/store.js
+++ src/store.js
@@ -7,43 +7,48 @@ import logger from 'redux-logger';
 const feelingResponse = (state = 0, action) => {
   if (action.type === 'SET_FEELING_RESPONSE') {
     return action.payload;
-  } else if (action.type === 'RESET_FEEDBACK') {
-    return (state = 0);
+    // } else if (action.type === 'RESET_FEEDBACK') {
+    //   return (state = 0);
   }
+  return state;
 };
 //Understanding = (state = number, action) => return state;
 const understandingResponse = (state = 0, action) => {
   if (action.type === 'SET_UNDERSTANDING_RESPONSE') {
     return action.payload;
-  } else if (action.type === 'RESET_FEEDBACK') {
-    return (state = 0);
+    // } else if (action.type === 'RESET_FEEDBACK') {
+    //   return (state = 0);
   }
+  return state;
 };
 //Support = (state = number, action) => return state;
 const supportResponse = (state = 0, action) => {
   if (action.type === 'SET_SUPPORT_RESPONSE') {
     return action.payload;
-  } else if (action.type === 'RESET_FEEDBACK') {
-    return (state = 0);
+    // } else if (action.type === 'RESET_FEEDBACK') {
+    //   return (state = 0);
   }
+  return state;
 };
 //Comments = (state = string, action) => return state;
-const commentsResponse = (state = '', action) => {
+const commentsResponse = (state = [], action) => {
   if (action.type === 'SET_COMMENTS_RESPONSE') {
     return action.payload;
-  } else if (action.type === 'RESET_FEEDBACK') {
-    return (state = 0);
+    // } else if (action.type === 'RESET_FEEDBACK') {
+    //   return (state = '');
   }
+  return state;
 };
 
 //STORE REDUCERS
-export const store = createStore(
-  combineReducers({
-    feelingResponse,
-    understandingResponse,
-    supportResponse,
-    commentsResponse,
-  }),
-  //MALWARE
-  applyMiddleware(logger)
-);
+export const store = () =>
+  createStore(
+    combineReducers({
+      feelingResponse,
+      understandingResponse,
+      supportResponse,
+      commentsResponse,
+    }),
+    //MALWARE
+    applyMiddleware(logger)
+  );
